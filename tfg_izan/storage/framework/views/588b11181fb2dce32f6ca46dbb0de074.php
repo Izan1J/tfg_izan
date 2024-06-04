7311c8c310e32b46afdf220df3d5c01b2dc3454d
Izan1J <izansorianocatral@gmail.com>
2024-06-05 00:01:26 +0200

    estoy hatrtdsjgsd

--- tfg_izan/storage/framework/views/588b11181fb2dce32f6ca46dbb0de074.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2024 Angel de la guarda</strong> Todos los derechos reservados.
-</footer><?php /**PATH C:\xampp\htdocs\tfg_izan\resources\views/layouts/partials/footer.blade.php ENDPATH**/ ?>
